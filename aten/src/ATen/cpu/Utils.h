64b6f0a7c536182c29ca89a18c9038a6235f493d
Jiong Gong <jiong.gong@intel.com>
2024-06-08 02:36:41 -0700

    [inductor][cpp] BF16 AMX micro-gemm support
    ghstack-source-id: 9a84cbc48ce59f7d30b0c0592884b9dce0aadc90
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/127195

--- aten/src/ATen/cpu/Utils.h
+++ aten/src/ATen/cpu/Utils.h
@@ -10,4 +10,7 @@ TORCH_API bool is_cpu_support_avx512();
 // Detect if CPU support Vector Neural Network Instruction.
 TORCH_API bool is_cpu_support_vnni();
 
+// Enable the system to use AMX instructions.
+TORCH_API bool init_amx();
+
 } // namespace at::cpu
